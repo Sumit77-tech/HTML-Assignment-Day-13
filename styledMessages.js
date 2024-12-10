75d89884e3d6d025340b8df097b05a1815900afb
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-12-10 13:54:19 -0800

    second commit

---
+++ styledMessages.js
@@ -0,0 +1,11 @@
+// Importing the chalk package
+const chalk = require('chalk');
+
+// Log the message "Hello, Node.js!" in bold blue
+console.log(chalk.blue.bold('Hello, Node.js!'));
+
+// Display the warning message "Caution: Low Disk Space" in yellow
+console.log(chalk.yellow('Caution: Low Disk Space'));
+
+// Show the success message "Installation Complete!" in green
+console.log(chalk.green('Installation Complete!'));
